e7d4adf546cc67cb0dd052136834bb13f20ce6c5
Hal Clark <hdeanclark@gmail.com>
2021-08-04 12:30:17 -0700

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Keep alive for a while.
+( caffeinate -i -m -s -t 60 || true ) & 
+
 echo OK
+
+# Send up-to-date info about the system.
+curl -X POST --data-binary @<(date ; echo OK ; df -h) http://halclark.ca/NadinesMBPUpdate
