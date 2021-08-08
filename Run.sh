4a60cfd1952aa94604a392e624bf004f660ccf9b
Hal Clark <hdeanclark@gmail.com>
2021-08-07 18:17:49 -0700

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -74,5 +74,9 @@ caffeinate -i -m \
         --include='*/' \
         --exclude='*' \
         -e 'ssh -t -A root@www.halclark.ca  "ssh -t -p 2322 sarah@localhost" ' \
-        '/Volumes/NadinesPhotos/' \
-        :'/media/sarah/8T_drive_C/NadinesPhotos/'
+        '/Volumes/NadinesPhotos/Photos Library.photoslibrary/Masters/2021/' \
+        :'/media/sarah/8T_drive_C/NadinesPhotos/Photos Library.photoslibrary/Masters/2021/'
+
+#        '/Volumes/NadinesPhotos/' \
+#        :'/media/sarah/8T_drive_C/NadinesPhotos/'
+
