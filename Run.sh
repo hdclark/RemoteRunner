762d479e3cd0bb58f1a31a328818c0c09a824f36
Hal Clark <hdeanclark@gmail.com>
2021-08-07 18:01:22 -0700

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-#
+if [ -z "$(hostname | grep -i Nadines-MBP)" ] ; then
+    exit 1
+fi
+
 # Keep alive for a while, hopefully overlapping with previous to keep remote system accessible.
 #
 ( caffeinate -i -m -s -t 60 || true ) & 
 
 
-#
 # Ensure the port-forwarding services are enabled and running.
 #
 # com.halclark.autosshportfwd
@@ -20,14 +22,12 @@
 #sudo launchctl print user/com.halclark.syncphotos
 #sudo launchctl kickstart -k system/com.openssh.sshd
 
-#
 # Send up-to-date info about the system.
 #
 ( set -x ; 
   date ;
   uptime ;
   df -h ) > /Users/NadineClark/Scripts/info.txt
-  
 curl -X POST --data-binary @/Users/NadineClark/Scripts/info.txt http://halclark.ca/NadinesMBPUpdate
 
 RSYNC="rsync"
@@ -41,7 +41,7 @@ fi
 caffeinate -i -m \
   $RSYNC \
         -rtvPL \
-        --rsync-path="nice -n 19 /usr/bin/rsync" \
+        --rsync-path="nice -n 19 $RSYNC" \
         --inplace \
         --bwlimit=150 \
         --include='*txt'  --include='*TXT' \
@@ -56,3 +56,23 @@ caffeinate -i -m \
         root@www.halclark.ca:'/root/NadinesMBP/'
 
 rm /Users/NadineClark/Scripts/info.txt
+
+# Sync photos.
+caffeinate -i -m \
+  $RSYNC \
+        -rtvPL \
+        --rsync-path="nice -n 19 $RSYNC" \
+        --inplace \
+        --ignore-existing \
+        --size-only \
+        --bwlimit=150 \
+        --include='*JPG'  --include='*MOV' \
+        --include='*jpg'  --include='*mov' \
+        --include='*PNG'  --include='*MP4' \
+        --include='*png'  --include='*mp4' \
+        --include='*HEIC' --include='*heic' \
+        --include='*/' \
+        --exclude='*' \
+        -e 'ssh -t -A root@www.halclark.ca  "ssh -t -p 2322 sarah@localhost" ' \
+        '/Volumes/NadinesPhotos/ \
+        :'/media/sarah/8T_drive_C/NadinesPhotos/'
